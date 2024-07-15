12fdad0f3d60fc135527644fee1c4e458148ca90
Oprea Marian <80275594+Icandoit89@users.noreply.github.com>
2024-07-15 15:37:52 +0100

    exercitiu

---
+++ 1.js
@@ -0,0 +1,27 @@
+document.getElementById("adauga").addEventListener("click", function() {
+    
+    var elementType = document.getElementById("element").value;
+    var textColor = document.getElementById("culoare-text").value;
+    var bgColor = document.getElementById("culoare-background").value;
+    
+    // Crează un nou element
+    var newElement;
+    if (elementType === "box" || elementType === "cerc") {
+        newElement = document.createElement("div");
+        newElement.style.width = "100px";
+        newElement.style.height = "100px";
+        newElement.style.backgroundColor = bgColor;
+        newElement.style.border = `2px solid ${textColor}`;
+        if (elementType === "cerc") {
+            newElement.style.borderRadius = "50%";
+        }
+    } else {
+        newElement = document.createElement(elementType);
+        newElement.style.color = textColor;
+        newElement.style.backgroundColor = bgColor;
+        newElement.textContent = `Acesta este un ${elementType} element.`;
+    }
+
+    
+    document.getElementById("parinte").appendChild(newElement);
+});
